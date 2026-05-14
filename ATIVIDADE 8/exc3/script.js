83681300e1787fb9c9bd35f7629d8e5d8cdee1a0
Apollo Maria <apollomscarvalho@gmail.com>
2026-05-14 11:28:20 -0300

    Add files via upload

--- ATIVIDADE 8/exc3/script.js
+++ ATIVIDADE 8/exc3/script.js
@@ -2,13 +2,25 @@ const botao = document.getElementById("botao");
 botao.addEventListener("click", calcular);
 
 function calcular() {
-    const nome = document.getElementById("nome");
+    const nome = document.getElementById("nome").value;
     const nota1 = parseFloat(document.getElementById("nota1").value);
     const nota2 = parseFloat(document.getElementById("nota2").value);
     const nota3 = parseFloat(document.getElementById("nota3").value);
-    const situ = document.getElementById("situacao");
+    const paragrafo = document.getElementById("situacao");
 
     const media = ((nota1 + nota2 + nota3)/3);
+    let situacao;
 
-    situ.innerHTML = media.toFixed(2);
+    if (media >= 7) {
+        situacao = "APROVADO";
+        paragrafo.classList.add("ap");
+    } else if (media >= 4) {
+        situacao = `EXAME (faltam: ${10-media.toFixed(2)} pontos)`;
+        paragrafo.classList.add("ex");
+    } else {
+        situacao = "REPROVADO";
+        paragrafo.classList.add("re");
+    }
+
+    paragrafo.innerHTML = `<span> Nome: ${nome}<br> Média: ${media.toFixed(2)}<br> Situação: ${situacao} </span>`;
 }
